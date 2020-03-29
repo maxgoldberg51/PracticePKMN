6d511f2ea56f3cf773a775b3982b001ae7399709
Max Goldberg <Max.J.Goldberg-1@ou.edu>
2020-03-28 21:44:17 -0700

    Completed classes

---
+++ PrcticePKMN/PrcticePKMN/AllPokemonAPI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrcticePKMN
+{
+    class AllPokemonAPI
+    {
+        public int count { get; set; }
+        public string next { get; set; }
+        public string previous { get; set; }
+
+        public List<AllPokemonResult> results { get; set; }
+    }
+
+    public class AllPokemonResult
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+}
---
+++ PrcticePKMN/PrcticePKMN/PokemonAPI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrcticePKMN
+{
+    class PokemonAPI
+    {
+        public int height { get; set; }
+        public int weight { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public Sprite sprites { get; set; }
+    }
+    public class Sprite
+    {
+
+        public string front_default { get; set; }
+        public string back_default { get; set; }
+    }
+}
